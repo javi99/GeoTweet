71018f6dd006b715bad170b2f5eebd8edfa579ef
PolBaladas <polbaladasluna@gmail.com>
2016-10-03 13:51:39 +0200

    Added Javi Folder

---
+++ GeoTweet-Pol/geo_tweet.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import sys 
+
+consumer_keys = [
+	"8zyzrvgdar320I9VMSHEflfRT", 
+	"a0Gjt5dHgXOxsHgw6NZWynK75SM4N4YUYfTP14a2RYENWchYrP"
+]
+
+def getTweets(query):
+	pass
+
+def parseTweets(json_tweets):
+	pass
+
+
+query=sys.argv[1]t
